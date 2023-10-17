857a17ffe32d6a42d15a3ed570ebdf8a9fcb36e3
Srgy Surkv <sss1024@gmail.com>
2023-10-17 17:31:32 +0300

    1

--- riffshareiii/ts/application/ui/rightmenu/menuitem.ts
+++ riffshareiii/ts/application/ui/rightmenu/menuitem.ts
@@ -1,3 +1,21 @@
 class MenuItem{
-	
+	anchor: TileAnchor;
+	label: TileText;
+	bg: TileRectangle;
+	constructor(){
+        this.build();
+	}
+	build(){
+		this.bg = { x: 0, y: 0, w: 5, h: 5, rx: 0.4, ry: 0.4, css: 'toolBarButtonCircle' };
+		this.label = {
+            x: 0, y: 0, text: 'svavpkavm iwv awv rawv'
+            , css: 'toolBarButtonLabel'
+        }
+        this.anchor = {
+            xx: 0, yy: 0, ww: 111, hh: 111, showZoom: zoomPrefixLevelsCSS[0].zoom, hideZoom: zoomPrefixLevelsCSS[10].zoom, content: [
+                this.bg
+                , this.label
+            ]
+        };
+	}
 }
