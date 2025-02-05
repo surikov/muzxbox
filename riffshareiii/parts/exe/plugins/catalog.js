2aeb77181dc1e35780830f12220a5527af8c208b
Srgy Surkv <sss1024@gmail.com>
2025-02-05 14:19:15 +0300

    parts

---
+++ riffshareiii/parts/exe/plugins/catalog.js
@@ -0,0 +1,38 @@
+"use strict";
+let _t_all_registerd_plugins_list = [
+    { label: 'Import MIDI from file', purpose: 'Action', kind: 'importmidifile', ui: './plugins/actions/midi/midimusicimport.html', evaluate: 'none', script: 'none' },
+    { label: 'Import Guitar Pro file', purpose: 'Action', kind: 'import345gp', ui: './plugins/actions/alphagpimport/guitartab.html', evaluate: 'none', script: 'none' },
+    { label: 'Export file', purpose: 'Action', kind: 'exportfile', ui: './plugins/actions/exp2fi/localselector.html', evaluate: 'none', script: 'none' },
+    { label: 'Import file', purpose: 'Action', kind: 'importfile', ui: './plugins/actions/exp2fi/localprochooser.html', evaluate: 'none', script: 'none' },
+    { label: 'Base Volume', purpose: 'Filter', kind: 'basevolume', ui: './plugins/filters/basevolume/volume1.html', evaluate: 'none', script: 'none' },
+    { label: 'Project info', purpose: 'Action', kind: 'projectstatistics', ui: './plugins/actions/projinfo/info.html', evaluate: 'none', script: 'none' },
+    {
+        label: 'Simple beep sound', purpose: 'Performer', kind: 'beep1', ui: './plugins/performers/simplebeep/uitester/beep1.html',
+        evaluate: 'newSimpleBeepImplementation', script: './plugins/performers/simplebeep/beeper/beep_plugin.js'
+    }, {
+        label: 'Zvoog DrumKit', purpose: 'Sampler', kind: 'zdrum1', ui: './plugins/samplers/zvoog_drum_kit/drmsui.html',
+        evaluate: 'newZvoogDrumKitImplementation', script: './plugins/samplers/zvoog_drum_kit/zvoogdrumkit_plugin.js'
+    }, {
+        label: 'Zvoog Volume', purpose: 'Filter', kind: 'zvolume1', ui: './plugins/filters/zvoog_volume/zvolume1.html',
+        evaluate: 'newZvoogVolumeImplementation', script: './plugins/filters/zvoog_volume/zvolume_plugin.js'
+    }, {
+        label: 'Zvoog Echo', purpose: 'Filter', kind: 'zvecho1', ui: './plugins/filters/zvoog_echo/zvconv.html',
+        evaluate: 'newZvoogEchoImplementation', script: './plugins/filters/zvoog_echo/zecho_plugin.js'
+    },
+    {
+        label: 'Zvoog Compressor', purpose: 'Filter', kind: 'zvooco1', ui: './plugins/filters/zvoog_compressor/zvoocomp.html',
+        evaluate: 'newZvoogCompreImplementation', script: './plugins/filters/zvoog_compressor/zc_plugin.js'
+    },
+    {
+        label: 'Zvoog Performer', purpose: 'Performer', kind: 'zinstr1', ui: './plugins/performers/zvoog_perf/perfui.html',
+        evaluate: 'newZvoogBasePerformerImplementation', script: './plugins/performers/zvoog_perf/zvoogperf_plugin.js'
+    },
+    {
+        label: 'Zvoog Strum', purpose: 'Performer', kind: 'zvstrumming1', ui: './plugins/performers/zvoog_strum/strumui.html',
+        evaluate: 'newZvoogStrumPerformerImplementation', script: './plugins/performers/zvoog_strum/zvoogstrum_plugin.js'
+    }
+];
+function MZXBX_currentPlugins() {
+    return _t_all_registerd_plugins_list;
+}
+//# sourceMappingURL=catalog.js.map
---
+++ riffshareiii/parts/exe/plugins/filters/zvoog_volume/zvolume_ui.d.ts
@@ -0,0 +1,281 @@
+declare type Zvoog_Metre = {
+    count: number;
+    part: number;
+};
+interface Zvoog_MetreMathType {
+    count: number;
+    part: number;
+    set(from: Zvoog_Metre): Zvoog_MetreMathType;
+    metre(): Zvoog_Metre;
+    simplyfy(): Zvoog_MetreMathType;
+    strip(toPart: number): Zvoog_MetreMathType;
+    equals(metre: Zvoog_Metre): boolean;
+    less(metre: Zvoog_Metre): boolean;
+    more(metre: Zvoog_Metre): boolean;
+    plus(metre: Zvoog_Metre): Zvoog_MetreMathType;
+    minus(metre: Zvoog_Metre): Zvoog_MetreMathType;
+    duration(tempo: number): number;
+    calculate(duration: number, tempo: number): Zvoog_MetreMathType;
+}
+declare type Zvoog_Slide = {
+    duration: Zvoog_Metre;
+    delta: number;
+};
+declare type Zvoog_PluginBase = {
+    setup: (audioContext: AudioContext) => boolean;
+};
+declare type Zvoog_PluginFilter = Zvoog_PluginBase | {
+    input: string;
+};
+declare type Zvoog_PluginPerformer = Zvoog_PluginBase | {
+    output: string;
+    schedule: (chord: Zvoog_Chord, when: number) => boolean;
+};
+declare type Zvoog_PluginSampler = Zvoog_PluginBase | {
+    output: string;
+};
+declare type Zvoog_FilterTarget = {
+    id: string;
+    kind: string;
+    data: string;
+    outputs: string[];
+    automation: Zvoog_FilterMeasure[];
+    iconPosition: {
+        x: number;
+        y: number;
+    };
+    state: 0 | 1;
+};
+declare type Zvoog_AudioSequencer = {
+    id: string;
+    data: string;
+    kind: string;
+    outputs: string[];
+    iconPosition: {
+        x: number;
+        y: number;
+    };
+    state: 0 | 1 | 2;
+};
+declare type Zvoog_AudioSampler = {
+    id: string;
+    data: string;
+    kind: string;
+    outputs: string[];
+    iconPosition: {
+        x: number;
+        y: number;
+    };
+    state: 0 | 1 | 2;
+};
+declare type Zvoog_Chord = {
+    skip: Zvoog_Metre;
+    pitches: number[];
+    slides: Zvoog_Slide[];
+};
+declare type Zvoog_TrackMeasure = {
+    chords: Zvoog_Chord[];
+};
+declare type Zvoog_PercussionMeasure = {
+    skips: Zvoog_Metre[];
+};
+declare type Zvoog_SongMeasure = {
+    tempo: number;
+    metre: Zvoog_Metre;
+};
+declare type Zvoog_FilterMeasure = {
+    changes: Zvoog_FilterStateChange[];
+};
+declare type Zvoog_FilterStateChange = {
+    skip: Zvoog_Metre;
+    stateBlob: string;
+};
+declare type Zvoog_PercussionTrack = {
+    title: string;
+    measures: Zvoog_PercussionMeasure[];
+    sampler: Zvoog_AudioSampler;
+    volume: number;
+};
+declare type Zvoog_MusicTrack = {
+    title: string;
+    measures: Zvoog_TrackMeasure[];
+    performer: Zvoog_AudioSequencer;
+    volume: number;
+};
+declare type Zvoog_CommentText = {
+    skip: Zvoog_Metre;
+    text: string;
+    row: number;
+};
+declare type Zvoog_CommentMeasure = {
+    points: Zvoog_CommentText[];
+};
+declare type Zvoog_Selection = {
+    startMeasure: number;
+    endMeasure: number;
+};
+declare type DifferenceCreate = {
+    kind: "+";
+    path: (string | number)[];
+    newNode: any;
+};
+declare type DifferenceRemove = {
+    kind: "-";
+    path: (string | number)[];
+    oldNode: any;
+};
+declare type DifferenceChange = {
+    kind: "=";
+    path: (string | number)[];
+    newValue: any;
+    oldValue: any;
+};
+declare type Zvoog_Action = DifferenceCreate | DifferenceRemove | DifferenceChange;
+declare type Zvoog_UICommand = {
+    position: {
+        x: number;
+        y: number;
+        z: number;
+    };
+    actions: Zvoog_Action[];
+};
+declare type Zvoog_Project = {
+    versionCode: '1';
+    title: string;
+    timeline: Zvoog_SongMeasure[];
+    tracks: Zvoog_MusicTrack[];
+    percussions: Zvoog_PercussionTrack[];
+    comments: Zvoog_CommentMeasure[];
+    filters: Zvoog_FilterTarget[];
+    selectedPart: Zvoog_Selection;
+    position: {
+        x: number;
+        y: number;
+        z: number;
+    };
+    list: boolean;
+    undo: Zvoog_UICommand[];
+    redo: Zvoog_UICommand[];
+};
+declare type MZXBX_CachedWave = {
+    path: string;
+    buffer: AudioBuffer | null;
+    canceled?: boolean;
+    line100?: number[];
+};
+declare type MZXBX_FilterHolder = {
+    plugin: MZXBX_AudioFilterPlugin | null;
+    filterId: string;
+    kind: string;
+    properties: string;
+};
+declare type MZXBX_PerformerSamplerHolder = {
+    plugin: MZXBX_AudioPerformerPlugin | MZXBX_AudioSamplerPlugin | null;
+    channelId: string;
+    kind: string;
+    properties: string;
+};
+declare type MZXBX_Channel = {
+    id: string;
+    performer: MZXBX_ChannelSource;
+    outputs: string[];
+};
+declare type MZXBX_SlideItem = {
+    duration: number;
+    delta: number;
+};
+declare type MZXBX_PlayItem = {
+    skip: number;
+    channelId: string;
+    pitches: number[];
+    slides: MZXBX_SlideItem[];
+};
+declare type MZXBX_FilterState = {
+    skip: number;
+    filterId: string;
+    data: string;
+};
+declare type MZXBX_Set = {
+    duration: number;
+    tempo: number;
+    items: MZXBX_PlayItem[];
+    states: MZXBX_FilterState[];
+};
+declare type MZXBX_Filter = {
+    id: string;
+    kind: string;
+    properties: string;
+    outputs: string[];
+};
+declare type MZXBX_AudioFilterPlugin = {
+    launch: (context: AudioContext, parameters: string) => void;
+    busy: () => null | string;
+    schedule: (when: number, tempo: number, parameters: string) => void;
+    input: () => AudioNode | null;
+    output: () => AudioNode | null;
+};
+declare type MZXBX_AudioSamplerPlugin = {
+    launch: (context: AudioContext, parameters: string) => void;
+    busy: () => null | string;
+    start: (when: number, tempo: number) => void;
+    cancel: () => void;
+    output: () => AudioNode | null;
+    duration: () => number;
+};
+declare type MZXBX_ChannelSource = {
+    kind: string;
+    properties: string;
+};
+declare type MZXBX_AudioPerformerPlugin = {
+    launch: (context: AudioContext, parameters: string) => void;
+    busy: () => null | string;
+    strum: (when: number, pitches: number[], tempo: number, slides: MZXBX_SlideItem[]) => void;
+    cancel: () => void;
+    output: () => AudioNode | null;
+};
+declare type MZXBX_Schedule = {
+    series: MZXBX_Set[];
+    channels: MZXBX_Channel[];
+    filters: MZXBX_Filter[];
+};
+declare type MZXBX_Player = {
+    startSetupPlugins: (context: AudioContext, schedule: MZXBX_Schedule) => string | null;
+    startLoop: (from: number, position: number, to: number) => string;
+    reconnectAllPlugins: (schedule: MZXBX_Schedule) => void;
+    cancel: () => void;
+    allFilters(): MZXBX_FilterHolder[];
+    allPerformersSamplers(): MZXBX_PerformerSamplerHolder[];
+    position: number;
+};
+declare type MZXBX_PluginRegistrationInformation = {
+    label: string;
+    kind: string;
+    purpose: 'Action' | 'Filter' | 'Sampler' | 'Performer';
+    ui: string;
+    evaluate: string;
+    script: string;
+};
+declare type MZXBX_MessageToPlugin = {
+    hostData: any;
+};
+declare type MZXBX_MessageToHost = {
+    dialogID: string;
+    pluginData: any;
+    done: boolean;
+};
+declare function MZXBX_waitForCondition(sleepMs: number, isDone: () => boolean, onFinish: () => void): void;
+declare function MZXBX_loadCachedBuffer(audioContext: AudioContext, path: string, onDone: (cachedWave: MZXBX_CachedWave) => void): void;
+declare function MZXBX_appendScriptURL(url: string): boolean;
+declare function MMUtil(): Zvoog_MetreMathType;
+declare function MZXBX_currentPlugins(): MZXBX_PluginRegistrationInformation[];
+declare class ZVUI {
+    id: string;
+    data: string;
+    slider: any;
+    init(): void;
+    sendMessageToHost(data: string): void;
+    receiveHostMessage(messageEvent: MessageEvent): void;
+    setMessagingId(newId: string): void;
+    setState(data: string): void;
+}
+declare function initZVUI(): void;
