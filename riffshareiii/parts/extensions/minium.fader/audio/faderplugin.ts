f1fadabb7daf27602913e8465d65a71ec1b17269
Srgy Surkv <sss1024@gmail.com>
2025-04-20 21:33:33 +0300

    1

---
+++ riffshareiii/parts/extensions/minium.fader/audio/faderplugin.ts
@@ -0,0 +1,31 @@
+console.log('zvolume v1.0');
+class ZVolImplementation implements MZXBX_AudioFilterPlugin {
+	audioContext: AudioContext;
+	volume: GainNode;
+	num: number = 0;
+	launch(context: AudioContext, parameters: string): void {
+		this.audioContext = context;
+		this.volume = this.audioContext.createGain();
+		//this.num = parseInt(parameters);
+		//this.volume.gain.setValueAtTime(this.num / 100, 0);
+		this.schedule(this.audioContext.currentTime + 0.001, 120, parameters);
+	}
+	busy(): null | string {
+		return null;
+	}
+	schedule(when: number, tempo: number, parameters: string): void {
+		this.volume.gain.setValueAtTime(this.num / 100, when);
+		this.num = parseInt(parameters);
+		this.volume.gain.linearRampToValueAtTime(this.num / 100, when + 0.001);
+		//console.log(this.audioContext.currentTime, when, tempo,parameters,this.num);
+	}
+	input(): AudioNode | null {
+		return this.volume;
+	}
+	output(): AudioNode | null {
+		return this.volume;
+	}
+}
+function newZvoogVolumeImplementation(): MZXBX_AudioFilterPlugin {
+	return new ZVolImplementation();
+}
---
+++ riffshareiii/parts/extensions/minium.fader/list.ts
@@ -0,0 +1,8 @@
+let pluginDebugInfo = {
+    "label": "Fader"
+    , purpose: 'Performer'
+    , kind: 'beep1'
+    , ui: './plugins/performers/simplebeep/uitester/beep1.html'
+    , evaluate: 'newSimpleBeepImplementation'
+    , script: './plugins/performers/simplebeep/beeper/beep_plugin.js'
+};
