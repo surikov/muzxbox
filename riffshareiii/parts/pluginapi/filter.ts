8987185094c8965a149b0d72135a045eac4c3d27
Srgy Surkv <sss1024@gmail.com>
2026-08-28 12:59:41 +0300

    1

--- riffshareiii/parts/pluginapi/filter.ts
+++
@@ -1,8 +0,0 @@
-
-type MZXBX_AudioFilterPlugin = {
-	launch: (context: AudioContext, parameters: string) => void;
-	busy: () => null | string;
-	schedule: (when: number, tempo: number, parameters: string) => void;
-	input: () => AudioNode | null;
-	output: () => AudioNode | null;
-};
--- riffshareiii/parts/pluginapi/registration.ts
+++
@@ -1,9 +0,0 @@
-
-type MZXBX_PluginRegistrationInformation = {
-	label: string
-	, kind: string
-	, purpose: 'Action' | 'Filter' | 'Sampler' | 'Performer'
-	, ui: string
-	, evaluate: string
-	, script: string
-};
